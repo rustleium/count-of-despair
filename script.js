3882187ffa5c22811471016502f4e41f32fd0f63
Rustam <189756814+rustleium@users.noreply.github.com>
2025-04-17 12:50:36 +0530

    feat: add functions to change despair

--- script.js
+++ script.js
@@ -2,4 +2,16 @@ console.log("We shall start... now");
 
 const DespairCounterLogic = (function () {
   let despair = 0;
+
+  function increaseDespair () {
+    despair ++;
+  }
+
+  function decreaseDespair () {
+    despair --;
+  }
+
+  function noDespair () {
+    despair = 0;
+  }
 })();
